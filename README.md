e1d9ff4542f1f1c373496ffd4ba017b8b4a927bb
colrichie <richmikan@richlab.org>
2021-03-13 21:21:49 +0900

    2021-03-13 version

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# TimingCmds_in_POSIX
+# Timing Management Commands in POSIX
+
+## What is this?
+
+This is a command set to make your UNIX life more convenient! On the current POSIX commands, are you satisfied with them for timing management? Unfortunately, we aren't. That is because POSIX doesn't release the commands for controling time accurately and/or precisely. For instance, image a scene you have to send text data at one second interval accurately. How can you do that? You can do only like that.
+
+```sh:
+cat /PATH/TO/textdata_source |
+while IFS= read -r line; do
+  printf '%s\n' "$line"
+  sleep 1
+done
+```
+
+However, that is not accurate because extra time is required to execute `while` ~ `done` sentence and `printf`. So, we made some commands to solve such problems.
+
+To solve the above problem, you can use `valve` command by the following.
+
+```sh:
+$ cat /PATH/TO/textdata_source | valve -l 1s
+```
+
+Several more commands are available.
+
+* [`calclock`](bin/calclock) ... Convert bewteen the Calendar time and UNIX time
+* [`getftimes`](c_src/getftimes) .. Display timestamps (mtime, ctime, atime) of a file
+* [`linets`](c_src/linets) ..... Add timestamp to every line of text data
+* [`ptw`](c_src/ptw) ........ A command wrapper to prevent a command from do full-buffering
+* [`sleep`](c_src/sleep) ...... Sleep command which supports sleeping during less than a second
+* [`tscat`](c_src/tscat) ...... Output each line at the data and time which is written in the top of the line
+* [`valve`](c_src/valve) ...... Output each byte/line at the specified interval
+
+To get the information for the commands, build the command and run them with the option `--help`.
+
+## How to Build
+
+First, `git clone` this repository. Then, run the `MAKE.sh` command to build the command with the source files.
+
+To short, all you have to do is to type the following commands.
+
+```sh:
+$ git clone https://github.com/NCNT/TimingCmds_in_POSIX.git
+$ c_src/MAKE.sh -d ../bin
+```
+
+And you can get the command in the `bin` directory. Copy or move them to one of directories which is listed in the `$PATH` variable.
+Enjoy!
+
+## Author / License
+
+Tomoyuki Matsuura as Colonel Richie, belongs to USP-NCNT project and ShellShoccar Japan.
+
+Everyting in this repository is completely free for everyone. If you want any license to use them by all means, we give you CC0 or the Unlicense. Anyway, take them freely as you like.
---
+++ c_src/getftimes.c
@@ -0,0 +1,159 @@
+/*####################################################################
+#
+# GETFTIMES - Get Timestamps of Each File
+#
+# USAGE   : getftimes [options] file [file ...]
+# Options : -l ... Prints the timestamps in ISO8601 format
+#           -u ... Prints the timestamps in UNIX time
+#           -- ... Finishes parsing arguments as options
+# Output  : * Print the following 4 fields by each file
+#             <atime> <mtime> <ctime> <filename>
+#           * The format of each time is either <YYYYMMDDhhmmss> or
+#             <YYYY-MM-DDThh:mm:ss+hhmm>.
+#           * The latter format is set by -l option.
+# Retuen  : Return 0 only when timestamps of all files were able to be
+#           gotten.
+#
+# How to compile : cc -O3 -o __CMDNAME__ __SRCNAME__
+#
+# Written by USP-NCNT prj. / Shell-Shoccar Japan (@shellshoccarjpn)
+#         on 2021-03-13
+#
+# This is a public-domain software (CC0). It means that all of the
+# people can use this for any purposes with no restrictions at all.
+# By the way, We are fed up with the side effects which are brought
+# about by the major licenses.
+#
+# The latest version is distributed at the following page.
+# https://github.com/ShellShoccar-jpn/misc-tools
+#
+####################################################################*/
+
+
+/*####################################################################
+# Initial Configuration
+####################################################################*/
+
+/*=== Initial Setting ==============================================*/
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <time.h>
+#include <sys/stat.h>
+
+char* gpszCmdname;
+
+/*=== Define the functions for printing usage and error ============*/
+void print_usage_and_exit(void) {
+  fprintf(stderr,
+    "Usage   : %s [options] file [file ...]\n"
+    "Options : -l ... Prints the timestamps in ISO8601 format\n"
+    "          -u ... Prints the timestamps in UNIX time\n"
+    "          -- ... Finishes parsing arguments as options\n"
+    "Output  : * Print the following 4 fields by each file\n"
+    "            <atime> <mtime> <ctime> <filename>\n"
+    "          * The format of each time is either <YYYYMMDDhhmmss> or\n"
+    "            <YYYY-MM-DDThh:mm:ss+hhmm>.\n"
+    "          * The latter format is set by -l option.\n"
+    "Retuen  : Return 0 only when timestamps of all files were able to be\n"
+    "          gotten. \n"
+    "Version : 2020-03-19 12:18:14 JST\n"
+    "          (POSIX C language)\n"
+    "\n"
+    "USP-NCNT prj. / Shell-Shoccar Japan (@shellshoccarjpn),\n"
+    "No rights reserved. This is public domain software. (CC0)\n"
+    "\n"
+    "The latest version is distributed at the following page.\n"
+    "https://github.com/ShellShoccar-jpn/misc-tools\n"
+    ,gpszCmdname);
+  exit(1);
+}
+void error_exit(int iErrno, const char* szFormat, ...) {
+  va_list va;
+  va_start(va, szFormat);
+  fprintf(stderr,"%s: ",gpszCmdname);
+  vfprintf(stderr,szFormat,va);
+  va_end(va);
+  exit(iErrno);
+}
+
+
+/*####################################################################
+# Main
+####################################################################*/
+
+int main(int argc, char *argv[]){
+
+  /*=== Initial Setting ============================================*/
+  struct stat stFileinfo;
+  struct tm*  pstTm;
+  char        szAtim[256], szMtim[256], szCtim[256], szFmt[256], szDummy[256];
+  int         iFmttype;    /* Long option switch */
+  int         i;           /* It means the argument position */
+  int         iNerror = 0; /* The number of error to get timestamps */
+
+  gpszCmdname = argv[0];
+  for (i=0; *(gpszCmdname+i)!='\0'; i++) {
+    if (*(gpszCmdname+i)=='/') {gpszCmdname=gpszCmdname+i+1;}
+  }
+  if (setenv("POSIXLY_CORRECT","1",1) < 0) {
+    error_exit(errno,"setenv() at initialization: \n", strerror(errno));
+  }
+
+  /*=== Parse options ==============================================*/
+  /*--- initialize option parameters -------------------------------*/
+  iFmttype = 0; /* 0:YYYYMMDDhhmmss 1:ISO8601 2:UnixTime */
+  /*--- get them ---------------------------------------------------*/
+  for (i=1; i<argc; i++) {
+    if        (strcmp (argv[i], "--"  )==0) {
+      i++;
+      break;
+    } else if (strcmp (argv[i], "-l"  )==0) {
+      iFmttype = 1;
+    } else if (strcmp (argv[i], "-u"  )==0) {
+      iFmttype = 2;
+    } else if (strncmp(argv[i], "-" ,1)==0) {
+      print_usage_and_exit();
+    } else {
+      break;
+    }
+  }
+  /*--- print the usage if no filename has given -------------------*/
+  if (i >= argc) { print_usage_and_exit(); }
+
+  /*=== Swtich to the long format mode if -l option has been set ===*/
+  switch (iFmttype) {
+    case  0: strcpy(szFmt  ,"%Y%m%d%H%M%S"       );
+             strcpy(szDummy,"%-14s %-14s %-14s " );
+             break;
+    case  1: strcpy(szFmt  ,"%Y-%m-%dT%H:%M:%S%z");
+             strcpy(szDummy,"%-24s %-24s %-24s " );
+             break;
+    case  2: strcpy(szFmt  ,"%s"                 );
+             strcpy(szDummy,"%-10s %-10s %-10s " );
+             break;
+    default: error_exit(1, "Unexpected Error!\n");
+  }
+
+  /*=== Main loop ==================================================*/
+  for (   ; i<argc; i++) {
+    if (stat(argv[i],&stFileinfo)==0) {
+      pstTm = localtime(&stFileinfo.st_atime); /* Not use st_*tim.tv_sec for */
+      strftime(szAtim, 256, szFmt, pstTm);     /* compatibility with earlier */
+      pstTm = localtime(&stFileinfo.st_mtime); /* versions */
+      strftime(szMtim, 256, szFmt, pstTm);
+      pstTm = localtime(&stFileinfo.st_ctime);
+      strftime(szCtim, 256, szFmt, pstTm);
+      printf("%s %s %s ",szAtim,szMtim,szCtim);
+    } else {
+      iNerror++;
+       printf(szDummy, "-", "-", "-");
+    }
+    printf("%s\n",argv[i]);
+  }
+
+  /*=== Finish =====================================================*/
+  return (iNerror==0) ? 0 : 1;
+}
